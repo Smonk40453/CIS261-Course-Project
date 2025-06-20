4d5ed0fc88f7c81ab990f30d6276d6856ed89a6c
username <useremail>
2025-06-20 07:19:18 -0700

    phase 4

--- CIS261_Course_Project.py
+++ CIS261_Course_Project.py
@@ -1,16 +1,29 @@
 #Stephanie Monk 
 #CIS261
 #Course Project 
-#Phase 3
+#Phase 4
 
 
 import datetime
-import os 
+import os
+
 FILENAME = "employee_data.txt"
+User_File = "users.txt"
+class Login:
+    def __init__(self,user_id,password,authorization):
+        self.user_id = user_id
+        self.password = password 
+        self.authorization = authorization
 def WelcomeMessage () : 
     print("\n Welcome to the Employee Payroll System")
 def GoodbyeMessage (): 
     print ("\n Thank you for using the payroll system. Goodbye.")
+def create_initial_users():
+    if not os.path.exists(User_File):
+        with open (User_File, "w") as file:
+            file.write("admin | admin123 | Admin\n")
+            file.write("user1 | userpass | User\n")
+        print("Default Admin and User accounts created.\n")
 def GetPositiveFloat(prompt) :
     while True:
         try:
@@ -106,20 +119,67 @@ def ReadAndDisplayFile():
                 summary['total_incomeTax'] += incomeTax
                 summary['total_netpay'] += netPay
     display_totals(summary)
-def main() :
-    WelcomeMessage()
-    with open (FILENAME, "a") as file:
+def load_user_data():
+    user_list =[]
+    if os.path.exists(User_File):
+       with open (User_File, "r") as file:
+           for line in file:
+               parts = line.strip().split("|")
+               if len(parts) == 3:
+                      user_list. append(parts)
+    return user_list
+def register_users():
+    users = load_user_data()
+    user_ids = [user[0] for user in users]
+    with open (User_File, "a") as file:
         while True:
-            print("Type 'End' to stop or enter employee name to continue: ")
-            name = GetEmployeeName()
-            if name.lower() =="end":
+            user_id = input("Enter new user ID(or 'End' to stop): ").strip()
+            if user_id.lower() == "end":
                 break 
-            from_date,to_date = GetDateRange()
-            hours = GetTotalhours()
-            rate = GetHourlyRate()
-            taxRate = GetTaxRate()
-            file.write(f"{from_date}|{to_date}|{name}|{hours}|{rate}|{taxRate}\n")
-    ReadAndDisplayFile()
+            if user_id in user_ids: 
+                print("User ID already exists.")
+                continue 
+            password = input("Enter password: ").strip()
+            auth = input("Enter authorization(Admin/User): ").strip()
+            if auth not in ["Admin","User"]:
+                print("Authorization must be 'Admin' or 'User'")
+                continue 
+            file.write(f"{user_id}|{password}|{auth}\n")
+            print(f"User {user_id} added.")
+def login_user():
+    users = load_user_data()
+    user_id = input("Enter your user ID: ").strip()
+    password = input("Enter your password: ").strip()
+    for user in users: 
+        if user_id == user[0] and password == user[1]:
+            print(f"\nLogin successful.Welcome,{user_id}!")
+            return Login(user_id, password, user[2])
+    else:
+        print("Invalid credientials. Exiting.")
+        exit()
+def main() :
+    create_initial_users()
+    WelcomeMessage()
+    user = login_user()
+    if user.authorization == "Admin":
+        register_choice = input("Do you want to register new users?(yes/no: ").lower()
+        if register_choice == "yes":
+            register_users()
+        with open (FILENAME, "a") as file:
+            while True:
+                print("Type 'End' to stop or enter employee name to continue: ")
+                name = GetEmployeeName()
+                if name.lower() =="end":
+                    break 
+                from_date,to_date = GetDateRange()
+                hours = GetTotalhours()
+                rate = GetHourlyRate()
+                taxRate = GetTaxRate()
+                file.write(f"{from_date}|{to_date}|{name}|{hours}|{rate}|{taxRate}\n")
+        ReadAndDisplayFile()
+    elif user.authorization == "User": 
+        print("\nViewing access granted.")
+        ReadAndDisplayFile()
     GoodbyeMessage()
 if __name__=="__main__":
     main()
